d8fa9487cc2af64a2f369adb80239f0a1aaef9fc
Nathan Loisel <nathan.loisel@apptitude.ch>
2017-07-27 18:00:59 +0200

    add style to empty app page

--- project-base/src/universal/features/routing/pages/HomePage/index.js
+++ project-base/src/universal/features/routing/pages/HomePage/index.js
@@ -8,6 +8,15 @@ import React from 'react';
 
 import styles from './styles.scss';
 
-const HomePage = () => <div className={styles.HomePage}>HomePage</div>;
-
+const HomePage = () => (
+  <div className={styles.HomePage}>
+    <div>
+      <h2>Made by</h2>
+      <h1>APPTITUDE</h1>
+      <a href="https://apptitude.ch" target="_blank" rel="noopener noreferrer">
+        apptitude.ch
+      </a>
+    </div>
+  </div>
+);
 export default HomePage;
--- project-base/src/universal/features/ui/app/App/index.js
+++ project-base/src/universal/features/ui/app/App/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
 
 import 'assets/style/font.scss';
-import config from 'appConfig';
 
 import styles from './styles.scss';
 
-const App = ({ children }) => <div className={styles.App}>This is an Application</div>;
+const App = ({ children }) => <div className={styles.App}>{children}</div>;
 
 App.propTypes = { children: PropTypes.element };
 
