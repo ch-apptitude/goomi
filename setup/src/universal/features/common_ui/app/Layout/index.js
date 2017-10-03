36ff8640781743b7da1f88a611b7c8cc1494d9d2
JeremyBarthoux <barthoux.jeremy@gmail.com>
2017-10-03 13:29:18 +0200

    Fixed grid - layout

--- setup/src/universal/features/common_ui/app/Layout/index.js
+++ setup/src/universal/features/common_ui/app/Layout/index.js
@@ -13,14 +13,27 @@ import Header from 'features/common_ui/app/Header';
 import Footer from 'features/common_ui/app/Footer';
 import Modal from 'features/common_ui/components/Modal';
 
-const LayoutContainer = styled.div`height: 100%;`;
+const LayoutContainer = styled.div`
+  height: 100%;
+  min-width: ${Theme.Sizes.xs};
+`;
+const StyledBody = styled.div`
+  height: calc(100% - ${Theme.Metrics.header_height}px);
+  padding-left: 0.5rem; // Fixed flexgrid : add container || Like <grid> fixed
+  padding-right: 0.5rem; // Fixed flexgrid : add container || Like <grid> fixed
+  box-sizing: border-box; // Fixed flexgrid : add container || Like <grid> fixed
+`;
 
-const StyledBody = styled.div`height: calc(100% - ${Theme.Metrics.header_height}px);`;
+const HeaderStyle = styled(Header)`
+  padding-left: 0.5rem; // Fixed flexgrid : add container || Like <grid> fixed
+  padding-right: 0.5rem; // Fixed flexgrid : add container || Like <grid> fixed
+  box-sizing: border-box; // Fixed flexgrid : add container || Like <grid> fixed
+`;
 
 const Layout = ({ children }) => (
   <LayoutContainer>
-    <Header />
-    <StyledBody>{React.Children.map(children, (child) => child)}</StyledBody>
+    <HeaderStyle />
+    <StyledBody>{React.Children.map(children, child => child)}</StyledBody>
     <Modal />
     <Footer />
   </LayoutContainer>
