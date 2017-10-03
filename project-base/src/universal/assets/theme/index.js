81539e10127838abbafdc392f388df19a6d334a8
JeremyBarthoux <barthoux.jeremy@gmail.com>
2017-10-03 13:28:11 +0200

    Update Theme

--- project-base/src/universal/assets/theme/index.js
+++ project-base/src/universal/assets/theme/index.js
@@ -1,7 +1,14 @@
 import Metrics from './metrics';
 import Colors from './colors';
+import { sizes } from './media';
+
+const sizeNew = {};
+Object.keys(sizes).forEach(size => {
+  sizeNew[size] = `${sizes[size]}em`;
+});
 
 export default {
   Metrics,
   Colors,
+  Sizes: sizeNew,
 };
